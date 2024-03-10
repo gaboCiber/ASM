0b2565a1b080f75a18b9231dd704af8171b89e83
gaboCiber <gabopla.lasa@gmail.com>
2024-03-10 17:57:48 -0400

    Solve Task 20 on assambly

---
+++ Task 20.asm
@@ -0,0 +1,39 @@
+%include "io.inc"
+
+section .data
+b db 0
+c db 2
+n dd 4
+L db 4, 3, 5, 6
+
+
+section .text
+global main
+main:
+    ; Obtener direccion L+b
+    mov ebx, L
+    add bl, [b]
+    
+    mov al, [ebx] ; max = [L+b]
+    mov cl, [b]
+    
+    While: ; b <= c
+    cmp cl, [c]
+    JA End
+    
+    cmp al, [ebx] ; if(max <= [b])
+    JA Incremet
+    mov al, [ebx] ; max = [b]
+    
+    Incremet: 
+    inc cl
+    inc ebx ; b++
+    
+    jmp While
+    
+    End:
+    PRINT_DEC 1, al
+    xor al, al
+    xor ebx, ebx
+    xor cl, cl
+    ret
